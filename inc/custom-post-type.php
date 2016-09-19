273bac89a561c8d35e2cd0257ca31bb82aefd0f1
kirandash <thekirankumardash@gmail.com>
2016-09-19 21:52:49 +0530

    s accumsan erat, id molestie tortor commodo a. In hac habitasse platea dictumst. Integer ut orci diam. Sed et sem vel felis interdum tempus vitae eget turpis. Duis rutrum ex justo, in pharetra justo tempus ut. Integer mi tellus, mollis quis turpis eget, vestibulum scelerisque ex. Vivamus ac tristique massa.

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -17,6 +17,8 @@ if( $contact == 1 ){
     // manage_yourpost/page/customposttype_posts_custom_column , this is a loop that goes through every post
     // 10 - action executes after everything is ready, 2 is the number of arguments to pass,default is 1
     add_action( 'manage_goa-contact_posts_custom_column', 'goa_theme_custom_contact_column', 10, 2 );
+    add_action( 'add_meta_boxes', 'goa_theme_contact_add_meta_box' );
+    add_action( 'save_post', 'goa_theme_save_contact_email_data' );
 }
 
 /* CONTACT CPT */
@@ -61,8 +63,48 @@ function goa_theme_custom_contact_column( $column, $post_id ) {
             
         case 'email':
             // email column
-            echo 'Email address';
+            $email = get_post_meta( $post_id, '_contact_email_value_key', true );
+            echo '<a href="mailto:'.$email.'">'.$email.'</a>';
             break;
             
     }
+}
+
+/* CONTACT META BOXES */
+function goa_theme_contact_add_meta_box() {
+    add_meta_box( 'contact_email', 'User Email', 'goa_theme_contact_email_callback', 'goa-contact', 'side', 'default' );
+}
+
+function goa_theme_contact_email_callback( $post ) {
+    // generate the nonce
+    wp_nonce_field( 'goa_theme_save_contact_email_data', 'goa_theme_contact_email_meta_box_nonce' );
+    
+    // retrieve the saved value
+    $value = get_post_meta( $post->ID, '_contact_email_value_key', true );
+    
+    // actual input of the meta box
+    echo '<label for="goa_theme_contact_email_field">User Email Address: </label>';
+    echo '<input type="email" id="goa_theme_contact_email_field" name="goa_theme_contact_email_field" value="'.esc_attr($value).'" size="25" />';
+}
+
+function goa_theme_save_contact_email_data( $post_id ) {
+    if( ! isset( $_POST['goa_theme_contact_email_meta_box_nonce'] ) ) {
+        return;
+    }
+    if( ! wp_verify_nonce( $_POST['goa_theme_contact_email_meta_box_nonce'], 'goa_theme_save_contact_email_data' ) ) {
+        return;
+    }
+    if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ){
+        return;
+        // remove this if you want the meta box also to autosave. better to not
+    }
+    if( ! current_user_can( 'edit_post', $post_id ) ) {
+        return;
+    }
+    if( ! isset( $_POST['goa_theme_contact_email_field'] ) ) {
+        return;
+    }
+    $my_data = sanitize_text_field( $_POST['goa_theme_contact_email_field'] );
+    
+    update_post_meta( $post_id, '_contact_email_value_key', $my_data );
 }
