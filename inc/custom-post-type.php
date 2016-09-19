fa7362d10fa6d66a72010569d2eda307bfc1c40b
kirandash <thekirankumardash@gmail.com>
2016-09-19 21:02:27 +0530

    Part 11 - Manage and Create Custom Columns

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -12,6 +12,11 @@ $contact = get_option( 'activate_contact' );
 
 if( $contact == 1 ){
 	add_action( 'init', 'goa_theme_contact_custom_post_type' );
+    // manage_yourpost/page/customposttype_posts_columns
+    add_filter( 'manage_goa-contact_posts_columns', 'goa_theme_set_contact_columns' );
+    // manage_yourpost/page/customposttype_posts_custom_column , this is a loop that goes through every post
+    // 10 - action executes after everything is ready, 2 is the number of arguments to pass,default is 1
+    add_action( 'manage_goa-contact_posts_custom_column', 'goa_theme_custom_contact_column', 10, 2 );
 }
 
 /* CONTACT CPT */
@@ -35,4 +40,29 @@ function goa_theme_contact_custom_post_type() {
 	);
 	
 	register_post_type( 'goa-contact', $args );
+}
+
+function goa_theme_set_contact_columns($columns) {
+    // unset( $columns['author'] );
+    $newColumns = array();
+    $newColumns['title'] = 'Full Name';
+    $newColumns['message'] = 'Message';
+    $newColumns['email'] = 'Email';
+    $newColumns['date'] = 'Date';
+    return $newColumns;
+}
+
+function goa_theme_custom_contact_column( $column, $post_id ) {
+    switch( $column ){
+        case 'message':
+            // message column
+            echo get_the_excerpt();
+            break;
+            
+        case 'email':
+            // email column
+            echo 'Email address';
+            break;
+            
+    }
 }
