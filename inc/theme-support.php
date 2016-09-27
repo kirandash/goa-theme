1dccde4c081a0e6286af271b54dd9a56c37b62b0
kirandash <thekirankumardash@gmail.com>
2016-09-27 20:58:14 +0530

    19 - Code the Standard Post Format

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -28,6 +28,8 @@ if($background==1){
 	add_theme_support('custom-background');
 }	
 
+add_theme_support( 'post-thumbnails' );
+
 /* Activate Nav Menu Option */
 function goa_theme_register_nav_menu() {
     register_nav_menu( 'primary','Header Navigation Menu' );
@@ -35,3 +37,29 @@ function goa_theme_register_nav_menu() {
 
 add_action( 'after_setup_theme', 'goa_theme_register_nav_menu' );
 
+/*
+	==============================
+		BLOG LOOP CUSTOM FUNCTIONS
+	==============================
+*/
+function goa_theme_posted_meta() {
+    $posted_on = human_time_diff( get_the_time('U'), current_time('timestamp') );
+    $categories = get_the_category();
+    $separator = ', ';
+    $output = '';
+    $i = 1;
+    
+    if( !empty($categories) ):
+        foreach( $categories as $category ) :
+            if( $i>1 ): $output .= $separator; endif;
+            $output .= '<a href="'. esc_url( get_category_link( $category->term_id ) ) .'" alt="'. esc_attr('View all posts in %s', $category->name ) .'">'. esc_html( $category->name ) .'</a>';
+            $i++;
+        endforeach;
+    endif;
+        
+    return '<span class="posted-on">Posted <a href="'. esc_url( get_permalink() ) .'">'. $posted_on .'</a>ago</span> / <span class="posted-in">'. $output .'</span>';
+}
+
+function goa_theme_posted_footer() {
+    return 'posted footer';
+}
--- index.php
+++ index.php
@@ -1,4 +1,32 @@
-<?php
-get_header();
-get_footer();
-?>
+<?php 
+/*
+    @package goa-theme
+*/
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        
+        <main id="main" class="site-main" role="main">
+            
+            <div class="container">
+                
+                <?php
+                    if( have_posts() ):
+                        
+                        while( have_posts() ): the_post();
+                
+                            get_template_part( 'template-parts/content', get_post_format() );
+                
+                        endwhile;
+                
+                    endif;
+                ?>
+                
+            </div>
+            
+        </main>
+        
+    </div><!-- #primary -->
+
+<?php get_footer(); ?>
